744c1e55b681bb82b310a12824609068c75ca354
Ben Tea <9259833+blueberryapple@users.noreply.github.com>
2019-08-31 22:40:49 -0700

    changed testSum method signature to be more concise

--- src/test/java/TestSolution.java
+++ src/test/java/TestSolution.java
@@ -13,18 +13,20 @@ public class TestSolution {
 
     @Test
     public void testAddTwoNumbers() {
-        testSum(5, 4, 1);
-        testSum(0, 0, 0);
-        testSum( 10, 5, 5);
-        testSum(18, 9, 9);
-        testSum(333, 185, 148);
-        testSum(1000, 500, 500);
-        testSum(1000, 999, 1);
+        testSum(4, 1);
+        testSum(0, 0);
+        testSum(5, 5);
+        testSum(9, 9);
+        testSum(185, 148);
+        testSum(500, 500);
+        testSum(999, 1);
+        testSum(1164, 70);
+        testSum(20999, 1);
     }
 
-    private void testSum(int sum, int summand1, int summand2) {
+    private void testSum(int summand1, int summand2) {
         Assert.assertEquals(
-                ListNode.fromInt(sum),
+                ListNode.fromInt(summand1 + summand2),
                 solution.addTwoNumbers(
                         ListNode.fromInt(summand1),
                         ListNode.fromInt(summand2)
